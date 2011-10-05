af9b82bfca1581b3f8111c794459e6751ff86385
Carlos Caballero <cijkb.j@gmail.com>
2011-10-05 15:27:08 -0400

    Trying re-design the books manipulation

--- data/sql/definition.sql
+++ data/sql/definition.sql
@@ -92,6 +92,3 @@ CREATE TABLE `babel_books_catalogs` (
     INDEX (`catalog`),
     FOREIGN KEY (`catalog`)       REFERENCES `babel_catalogs`(`ident`) ON UPDATE CASCADE ON DELETE CASCADE
 ) DEFAULT CHARACTER SET UTF8;
-
-INSERT INTO `babel_users` (`ident`, `fullname`, `username`, `password`, `avatar`) VALUES
-(1, 'Carlos Caballero',  'jacobian', '63ca26f56c5730ede6b21c7b681b917e2fb37765', 0);
---
+++ data/sql/install.sql
@@ -0,0 +1,9 @@
+\. definition.sql
+\. users/default.sql
+
+/* English */
+/* \. catalogs/en/dewey_code.sql */
+/* Español */
+\. catalogs/es/codificacion_dewey.sql
+\. catalogs/es/sistemas.sql
+\. catalogs/es/informatica.sql
