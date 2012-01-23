88f6593063017f683866835dc1a2bdeba99815cf
Carlos E. Caballero B <cijkb.j@gmail.com>
2012-01-23 16:13:18 +0000

    [MISC] Change the files positions

--- data/chmod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Change for directories
-find . -type d -exec chmod 755 -c {} \;
-
-#Change for files
-find . -type f -exec chmod 644 -c {} \;
-
--- data/find.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -type f -name '*[^.pdf]'
-
