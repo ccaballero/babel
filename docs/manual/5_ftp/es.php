e71bfc9b44a80db8cc7e99d9c94ec8e10067dfb6
Carlos Caballero B <cijkb.j@gmail.com>
2012-06-29 17:29:50 -0400

    [MANUAL] Added the screenshoots in linux ftp clientes

--- docs/manual/5_ftp/es.php
+++ docs/manual/5_ftp/es.php
@@ -1,6 +1,66 @@
 <h1>FTP</h1>
 
-<h2>Cyberduck</h2>
+<h2>Clientes FTP en linux</h2>
+
+<h3>FileZilla</h3>
+<div class="img">
+<img src="/media/img/help/ftp.filezilla.1.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.filezilla.2.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.filezilla.3.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.filezilla.4.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.filezilla.5.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.filezilla.6.png" alt="" title="" />
+<p></p>
+</div>
+
+<h3>ncftp</h3>
+<div class="img">
+<img src="/media/img/help/ftp.ncftp.1.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.ncftp.2.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.ncftp.3.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.ncftp.4.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.ncftp.5.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.ncftp.6.png" alt="" title="" />
+<p></p>
+</div>
+<div class="img">
+<img src="/media/img/help/ftp.ncftp.7.png" alt="" title="" />
+<p></p>
+</div>
+
+<h2>Clientes FTP en windows</h2>
+
+<h3>Cyberduck</h3>
 <div class="img">
 <img src="/media/img/help/ftp.cyberduck.1.png" alt="" title="" />
 <p></p>
@@ -26,7 +86,7 @@
 <p></p>
 </div>
 
-<h2>Total Commander</h2>
+<h3>Total Commander</h3>
 <div class="img">
 <img src="/media/img/help/ftp.totalcommander.1.png" alt="" title="" />
 <p></p>
@@ -60,7 +120,7 @@
 <p></p>
 </div>
 
-<h2>WinSCP</h2>
+<h3>WinSCP</h3>
 <div class="img">
 <img src="/media/img/help/ftp.winscp.1.png" alt="" title="" />
 <p></p>
