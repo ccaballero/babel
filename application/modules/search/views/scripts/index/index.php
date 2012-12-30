466e9572799374b7d1044644558b3a1185bd343c
Carlos Caballero B <cijkb.j@gmail.com>
2012-12-30 17:51:10 -0400

    [PHOTOS] Changed the standard for photographs nomination

--- application/modules/search/views/scripts/index/index.php
+++ application/modules/search/views/scripts/index/index.php
@@ -1,13 +1,13 @@
 <div id="form"><?php echo $this->form ?></div>
 
-<?php if (count($this->books) <> 0) { ?>
 <div id="list">
+<?php if (count($this->books) <> 0) { ?>
     <?php foreach($this->books as $book) { ?>
         <?php echo $this->partial('books/views/scripts/book.php', array('book' => $book, 'translate' => $this->translate)) ?>
     <?php } ?>
-</div>
 <?php } else { ?>
-    <p><?php echo $this->translate->_('No results found') ?></p>
+    <p class="description"><?php echo $this->translate->_('No results found') ?></p>
 <?php } ?>
+</div>
 
 <?php echo $this->partial('books/views/scripts/info.php', array('translate' => $this->translate)) ?>
